0d6c88ac1350a97d9371db05a851578a13f56a26
Maxine <maxinemeurer@yahoo.com>
2021-02-10 19:27:24 -0500

    commit tree a12512876f3bf25cca88097e8a7d87dcfc9e2cc2

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
+class Book
+    #to simplify everything you can write the longer getters/setters like this:
+    attr_accessor :author, :page_count, :genre,
+    attr_reader :title
+    
+    def initialize(title)
+        @title = title
+    end
 
+    #def title
+     #   @title
+   # end
+#author
+    #def author=(author)
+     #   @author = author
+    #end 
+
+    #def author
+     #   @author
+   # end
+#page_count
+   # def page_count=(page_count)
+      #  @page_count = page_count
+    #end
+
+    #def page_count
+    #    @page_count
+    #end
+#genre
+    #def genre=(genre)
+     #   @genre = genre
+    #end
+
+   #def genre
+    #    @genre
+    #end
+#turn the page
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,47 @@
-# Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+#color
+    def color=(color)
+        @color = color
+    end
+
+    def color
+        @color
+    end
+#size
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+#material
+    def material=(material)
+        @material = material
+    end
+
+    def material
+        @material
+    end
+#condition
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def condition
+        @condition
+    end
+#cobble
+    def cobble
+         puts "Your shoe is as good as new!"
+         @condition = "new"
+    end
+
+end
